7513768ad0511bf87ce6e5c9a921d99b526eb301
eri5000 <107961194+eri5000@users.noreply.github.com>
2023-08-10 17:02:50 -0600

    Add files via upload

---
+++ Kwitter_page.js
@@ -0,0 +1,17 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBt3rqjjvR3LjxFqJG5I7jSnSw5Buz0ies",
+    authDomain: "chatonline-cd0aa.firebaseapp.com",
+    databaseURL: "https://chatonline-cd0aa-default-rtdb.firebaseio.com",
+    projectId: "chatonline-cd0aa",
+    storageBucket: "chatonline-cd0aa.appspot.com",
+    messagingSenderId: "1048119489884",
+    appId: "1:1048119489884:web:d77c7a589bd9b7fd55fd99"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  user_name=localStorage.getItem("user_name");
+  room_name=localStorage.getItem("room_name");
+  document.getElementById("user_name").innerHTML="Hola " + user_name;
+  
